05d911fd61fbadb3dda59bd5e5c0ba0978ffb358
er1x <erixcbr@gmail.com>
2014-08-05 19:19:59 +0200

    model && view

--- battleship.js
+++ battleship.js
@@ -1,42 +1,60 @@
-// Battleship Game
+// Battleship Game v2
 // From Head First JavaScript
 
-console.log('Initializing Battleship');
+var view  = {
 
-// Variable declarations
+  displayMessage: function (msg){
+    document.getElementById('messageArea').innerHTML = msg;
+  },
 
-var randomLoc = Math.floor(Math.random() * 5);
+  displayHit: function(cell) {
+    document.getElementById(cell).setAttribute('class', 'hit');
+  },
 
-var location1 = randomLoc;
-var location2 = location1 + 1;
-var location3 = location2 + 1;
-
-var guess;
-var hits = 0;
-var guesses = 0;
-
-var isSunk = false;
-
-// Game Loop
-
-while(!isSunk) {
-  guess = prompt('Guess!');
-
-  if(guess < 0 || guess > 6) {
-    alert('Use a valid number!');
-  } else {
-    guesses += 1;
-    if(guess == location1 || guess == location2 || guess == location3) {
-      alert('Hit!');
-      hits += 1;
-      if(hits == 3){
-        isSunk = true;
-        alert('You sank my battleship!');
-      }
-    } else {
-      alert('Miss');
-    }
+  displayMiss: function(cell) {
+    document.getElementById(cell).setAttribute('class', 'miss');
   }
-}
 
-alert('Game finished. Total guesses = ' + guesses);
+};
+
+var model = {
+
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+          { locations: ["24", "34", "44"], hits: ["", "", ""] },
+          { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+
+  fire: function(guess) {
+    for (var i = 0; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var index = ship.locations.indexOf(guess);
+      if(index !== -1) {
+        ship.hits[index] = 'hit';
+        view.displayHit(guess);
+        view.displayMessage('HIT!');
+        if (this.isSunk(ship)) {
+          view.displayMessage('You sank my battleship!');
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage('You missed!');
+    return false;
+  },
+
+  isSunk: function(ship) {
+    for (var i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== 'hit') {
+        return false;
+      }
+    }
+    return true;
+  }
+
+};
