8bcace48f942488e5ff89eeeadb3e7b32d6c111e
er1x <erixcbr@gmail.com>
2014-08-05 20:53:52 +0200

    controller logic

--- battleship.js
+++ battleship.js
@@ -58,3 +58,68 @@ var model = {
   }
 
 };
+
+function parseGuess(guess) {
+
+  var parsedGuess = null;
+  var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+  var row, column;
+
+  if(guess.length === 2) {
+    row = alphabet.indexOf(guess[0]);
+    column = guess[1];
+    if(!isNaN(row) &&
+       !isNaN(column) &&
+       row >= 0 &&
+       column >= 0 &&
+       row < model.boardSize &&
+       column < model.boardSize) {
+         parsedGuess = row + column;
+       }
+  }
+
+  return parsedGuess;
+}
+
+var controller = {
+
+  guesses: 0,
+
+  processGuess: function(guess) {
+    var location = parseGuess(guess);
+    if(location) {
+      this.guesses++;
+      var hit = model.fire(location);
+      if(hit && model.shipsSunk === model.numShips) {
+        view.displayMessage('You sank all my battleships, in ' +
+                                      this.guesses + ' guesses');
+      }
+    }
+  }
+};
+
+function init() {
+  var fireButton = document.getElementById('fireButton');
+  fireButton.onclick = handleFireButton;
+
+  var guessInput = document.getElementById('guessInput');
+  guessInput.onkeypress = handleKeyPress;
+}
+
+function handleFireButton() {
+  var guessInput = document.getElementById('guessInput');
+  var guess = guessInput.value;
+
+  controller.processGuess(guess);
+  guessInput.value = '';
+}
+
+function handleKeyPress(e) {
+  var fireButton = document.getElementById('fireButton');
+  if (e.keyCode === 13) {
+    fireButton.click();
+    return false;
+  }
+}
+
+window.onload = init;
